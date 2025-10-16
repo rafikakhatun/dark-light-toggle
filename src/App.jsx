0477e678847153a1aabfd68a3f6807c850237131
rafika khatun <rafikakhatun607@gmail.com>
2025-10-16 20:19:38 +0530

    Add About section with gradient text and button

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,16 @@ function App() {
       {/* here dark light toggle button*/}
 
       <div className='flex justify-end p-6'>
-        <button className='bg-gray-200 p-4 rounded-full text-gray-700 text-xl hover:'><FaMoon /></button>
+        <button className='bg-gray-200 p-4 rounded-full text-gray-700 text-xl hover:bg-gray-300 cursor-pointer'><FaMoon /></button>
       </div>
+
+
 
       {/*Hero Section Text*/}
 
       <div className='px-8 py-12 space-y-5'>
         <h1 className='text-2xl md:text-3xl bg-gradient-to-l from-violet-700 to-pink-700 text-transparent bg-clip-text font-semibold'>Experience the Future of Web Design</h1>
-        <p className='text-md md:text-lg text-left'>Switch effortlessly between light and dark themes with a single click.
+        <p className='text-md md:text-lg text-left text-slate-700'>Switch effortlessly between light and dark themes with a single click.
           This project is built using React and Tailwind CSS, showcasing how
           modern UI development can feel smooth, elegant, and accessible for every user.
         </p>
@@ -43,7 +45,26 @@ function App() {
         </div>
       </div>
 
-     
+      {/*About Section Text */}
+
+      <div className='px-8 py-12 space-y-5 bg-gray-100'>
+        <h1 className='text-2xl md:text-3xl bg-gradient-to-l from-violet-700 to-pink-700 text-transparent bg-clip-text font-semibold'>About This Project</h1>
+        <p className='text-md md:text-lg text-left text-slate-700'>This Dark & Light Mode Theme Switcher is a React-based UI experiment
+          that demonstrates how easy it is to manage theme states using Tailwind CSS
+          and context-based toggles. The goal is to create a clean, responsive, and
+          user-friendly interface that adapts to your preference — anytime, anywhere.
+        </p>
+
+        {/*button*/}
+        <div>
+          <button className='px-4 py-3 bg-violet-600 rounded-md text-white cursor-pointer'>
+            VIEW SOURCE CODE
+          </button>
+        </div>
+
+      </div>
+
+
 
 
 
