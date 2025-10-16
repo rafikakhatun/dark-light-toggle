4ce7ba79afc11f9ba10f5d2cc9e8d797f74a7d15
rafika khatun <rafikakhatun607@gmail.com>
2025-10-16 20:11:26 +0530

    feat: add hero section and moon button UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { FaMoon } from 'react-icons/fa';
 
 
 
@@ -11,8 +12,8 @@ function App() {
 
   useEffect(() => {
     AOS.init({
-      duration: 1000, // animation duration (1 second)
-      once: true, // animation will happen only once
+      duration: 1000,
+      once: true,
     });
   }, []);
 
@@ -20,8 +21,32 @@ function App() {
   return (
     <>
 
-      {/*header (navbar) */}
-      nav
+      {/* here dark light toggle button*/}
+
+      <div className='flex justify-end p-6'>
+        <button className='bg-gray-200 p-4 rounded-full text-gray-700 text-xl hover:'><FaMoon /></button>
+      </div>
+
+      {/*Hero Section Text*/}
+
+      <div className='px-8 py-12 space-y-5'>
+        <h1 className='text-2xl md:text-3xl bg-gradient-to-l from-violet-700 to-pink-700 text-transparent bg-clip-text font-semibold'>Experience the Future of Web Design</h1>
+        <p className='text-md md:text-lg text-left'>Switch effortlessly between light and dark themes with a single click.
+          This project is built using React and Tailwind CSS, showcasing how
+          modern UI development can feel smooth, elegant, and accessible for every user.
+        </p>
+
+        {/*button section*/}
+        <div className='flex gap-4'>
+          <button className='px-6 py-3 bg-violet-600 rounded-md text-white cursor-pointer'>TRY DEMO</button>
+          <button className='px-6 py-3 bg-pink-600 rounded-md text-white cursor-pointer'>LEARN MORE</button>
+        </div>
+      </div>
+
+     
+
+
+
 
 
 
